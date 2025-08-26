fd57c83b39407e98ab49d5adc74350e448a16b78
Renato <ti.rp301223@gmail.com>
2025-08-25 22:38:21 -0300

    1

--- firebase.js
+++ firebase.js
@@ -2,14 +2,14 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-analytics.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+import { getAuth, signInAnonymously, updateProfile } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
 
-// Configuração do Firebase (copiada do console do Firebase)
+// Configuração do Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyDM-cWulKkn5RAT6tns0BktHkRdfFt4-0s",
   authDomain: "roteiro-a6cb0.firebaseapp.com",
   projectId: "roteiro-a6cb0",
-  storageBucket: "roteiro-a6cb0.appspot.com", // ✅ corrigido
+  storageBucket: "roteiro-a6cb0.appspot.com",
   messagingSenderId: "304848084937",
   appId: "1:304848084937:web:21c4d4ea2dc862e7e05218",
   measurementId: "G-PLMNPQWVH9"
@@ -23,4 +23,4 @@ const analytics = getAnalytics(app);
 const db = getFirestore(app);
 const auth = getAuth(app);
 
-export { app, analytics, db, auth };
+export { app, analytics, db, auth, signInAnonymously, updateProfile };
